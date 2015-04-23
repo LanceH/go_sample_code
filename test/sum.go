093184c90586160fa173bb818754576ec38180cc
Lance Hampton <lhampton@intellicentrics.com>
2015-04-23 14:50:52 -0500

    Updated test example to comply with golint.

--- test/sum.go
+++ test/sum.go
@@ -6,6 +6,9 @@ func main() {
   fmt.Println([4]int{1,2,3,4})
 }
 
+/*
+Sum returns the sum of all the elements of an array.
+*/
 func Sum(a []int) (s int){
   for _, i := range a {
     s += i
@@ -13,6 +16,9 @@ func Sum(a []int) (s int){
   return s
 }
 
+/*
+Average returns the average of all the elements of an array.
+*/
 func Average(a []int) (s float64){
   for _, i := range a {
     s += float64(i)
