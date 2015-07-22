9ceaa29039a45602983bd883b027b4392b73bbd7
Lance Hampton <lance.hampton@gmail.com>
2015-07-22 15:51:42 -0500

    Added an example that provides a 'go vet' warning and a function that will trip a warning.

---
+++ vet.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+Run from this directory:
+go tool vet .
+
+The fmt.Printf statement below is written to provide
+a go vet warning due to a mismatch of %s asking for a
+string and 33 being an int.
+*/
+
+import("fmt")
+
+func main() {
+  fmt.Printf("%s", 33)
+}
