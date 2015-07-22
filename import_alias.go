67a70ab90b20268ec332269df19c31d186267793
Lance Hampton <lance.hampton@gmail.com>
2015-07-22 14:50:49 -0500

    Added explanation of the golint dot import warning.

--- import_alias.go
+++ import_alias.go
@@ -3,6 +3,10 @@ package main
 /*
 import fmt directly to main (no 'fmt' prefix)
 alias the math package to 'm'
+
+This will result in a golint error for the . import.  It is not
+recommended to do a dot import because it may cause conflicts with
+future versions of go.
 */
 
 import (
