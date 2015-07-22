2c1e50c6ca71c76918eb245152d28369b0ed7078
Lance Hampton <lance.hampton@gmail.com>
2015-07-22 14:19:29 -0500

    Spacing to make channel.go look nicer.

--- channel.go
+++ channel.go
@@ -16,6 +16,7 @@ import(
 func main() {
   a := make(chan int)
   go handle(a) // make a go routine to wait around to do something with stuff put into a
+
   a <- 7 // put something into the channel
   time.Sleep(time.Second * 1) // this is here to make sure main() doesn't finish before handle()
 }
