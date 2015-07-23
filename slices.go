d7127fe98821c36aaf8bc3bf298ca945cdb1a2be
Lance Hampton <lance.hampton@gmail.com>
2015-07-23 10:55:36 -0500

    Added template of slices.go, no code yet.

---
+++ slices.go
@@ -0,0 +1,17 @@
+package main
+
+/*
+From "Effective Go":
+
+  Slices wrap arrays to give a more general, powerful, and convenient interface to sequences of data.
+
+Slices are generally the idiomatic way of doing stuff normally done in arrays in other languages.
+*/
+
+import ("fmt")
+
+
+
+func main() {
+  fmt.Println("TODO")
+}
