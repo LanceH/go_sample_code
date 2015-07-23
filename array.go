b7c975f54fa1bdde8c416e2ae3321c41a5e36fb8
Lance Hampton <lance.hampton@gmail.com>
2015-07-23 10:36:02 -0500

    Added basic array example.

---
+++ array.go
@@ -0,0 +1,42 @@
+package main
+
+/*
+From "Effective Go"
+
+    - Arrays are values. Assigning one array to another copies all the elements.
+    - In particular, if you pass an array to a function, it will receive a copy of the array, not a pointer to it.
+    - The size of an array is part of its type. The types [10]int and [20]int are distinct.
+
+Arrays are very strict and limited and are used internally to build slices.
+
+They are still useful for exact control of memory.
+*/
+
+import (
+	"fmt"
+)
+
+func main() {
+	// array initialized to values that the base type would initialized to
+	var a = [3]int64{}
+	for i, j := range a {
+		fmt.Printf("index: %d - value: %d\n\n", i, j)
+	}
+
+	// values are copied during assignment -- b doesn't point to the same memory as a
+	b := a
+	b[0] = 1
+	b[1] = 2
+	b[2] = 3
+	fmt.Printf("a[2]: %d - b[2]: %d\n\n", a[2], b[2])
+
+	// passing an array to a function copies it
+	// changes in that function don't affect the original
+	copy_test(a)
+	fmt.Printf("a[0] is unchanged: %d\n\n", a[0])
+}
+
+// the size of the array is part of the type and must be specified
+func copyTest(c [3]int64) {
+	c[0] = 186000
+}
