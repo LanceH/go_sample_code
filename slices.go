e9b008c4192c4fc0d379b4a25c1aac1bab07af38
Lance Hampton <lance.hampton@gmail.com>
2015-07-24 10:27:38 -0500

    Added examples to slices.go

--- slices.go
+++ slices.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 From "Effective Go":
 
@@ -8,10 +10,47 @@ From "Effective Go":
 Slices are generally the idiomatic way of doing stuff normally done in arrays in other languages.
 */
 
-import ("fmt")
-
-
+var a []int64
 
 func main() {
-  fmt.Println("TODO")
+	// a is a slice initialized as nil: []
+	// the capacity of the slice is doubled when it needs more
+	// room in the underlying array
+	fmt.Printf("%s has length: %d and capacity %d\n", a, len(a), cap(a))
+	a = append(a, 55)
+	fmt.Printf("%d has length: %d and capacity %d\n", a, len(a), cap(a))
+	a = append(a, 55)
+	fmt.Printf("%d has length: %d and capacity %d\n", a, len(a), cap(a))
+	a = append(a, 55)
+	fmt.Printf("%d has length: %d and capacity %d\n", a, len(a), cap(a))
+	a = append(a, 55)
+	fmt.Printf("%d has length: %d and capacity %d\n", a, len(a), cap(a))
+	a = append(a, 55)
+	fmt.Printf("%d has length: %d and capacity %d\n", a, len(a), cap(a))
+	a = append(a, 55)
+	fmt.Printf("%d has length: %d and capacity %d\n", a, len(a), cap(a))
+	a = append(a, 55)
+	fmt.Printf("%d has length: %d and capacity %d\n", a, len(a), cap(a))
+	a = append(a, 55)
+	fmt.Printf("%d has length: %d and capacity %d\n", a, len(a), cap(a))
+	a = append(a, 55)
+	fmt.Printf("%d has length: %d and capacity %d\n\n", a, len(a), cap(a))
+
+	// size of a slice can be described upon initialization using make()
+	b := make([]int64, 100, 1000)
+	fmt.Printf("%d has length: %d and capacity %d\n\n", b, len(b), cap(b))
+
+	// make an array
+	// then make a slice reference the exact same array
+	array := [3]int64{1, 2, 3}
+	slice := array[:]
+	fmt.Printf("%d has length: %d and capacity %d\n\n", slice, len(slice), cap(slice))
+	// this changes the underlying array
+	slice[2] = 12
+	fmt.Printf("%d has length: %d and capacity %d\n\n", slice, len(slice), cap(slice))
+
+	// once we append and increase the capacity, it now points to a different underlying array
+	slice = append(slice, 4)
+	fmt.Printf("%d has length: %d and capacity %d\n\n", slice, len(slice), cap(slice))
+	fmt.Println(array)
 }
